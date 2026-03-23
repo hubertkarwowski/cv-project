63eb939e836b0eaff315aa235bf72221f05cd00a
KamiiilPawlak <kami.patryk.pawlak@gmail.com>
2026-03-23 23:08:00 +0100

    style: fix join now button hover and remove borders

--- frontend/src/components/ui/Header.tsx
+++ frontend/src/components/ui/Header.tsx
@@ -6,24 +6,24 @@ import { cn } from '@/lib/utils.ts';
 import { Button } from './button';
 
 const headerVariants = cva(
-  'flex w-full items-center justify-between px-7 transition-all duration-700',
+  'flex w-full items-center justify-end px-6 transition-all duration-800',
   {
     variants: {
       variant: {
         default:
-          'border-b bg-background/80 backdrop-blur-md border-border sticky top-0 z-50',
+          'border-b bg-background/80 backdrop-blur-md border-border fixed top-0 z-50 p-10',
         transparent: 'bg-transparent border-transparent absolute top-0 z-50',
       },
 
       size: {
         default: 'h-16',
         sm: 'h-14',
-        lg: 'h-20',
+        lg: 'h-20 text-base',
       },
     },
     defaultVariants: {
-      variant: 'transparent',
-      size: 'default',
+      variant: 'default',
+      size: 'lg',
     },
   }
 );
@@ -35,61 +35,63 @@ interface HeaderProps
 
 function Header({ className, variant, size, ...props }: HeaderProps) {
   return (
-    <header
-      className={cn(headerVariants({ variant, size, className }))}
-      {...props}
-    >
-      {/* logo section */}
-      <div className="flex items-center gap-2 select-none">
-        <div className="bg-primary text-primary-foreground flex size-8 items-center justify-center rounded-lg font-bold">
-          C
+    <div className="">
+      <header
+        className={cn(headerVariants({ variant, size, className }))}
+        {...props}
+      >
+        {/* logo section */}
+        <div className="flex items-center gap-2 text-lg select-none md:text-xl">
+          <div className="bg-primary text-primary-foreground flex size-8 items-center justify-center rounded-lg font-bold md:size-8">
+            C
+          </div>
+          <span className="text-foreground hidden font-bold tracking-tight sm:block">
+            Future
+          </span>
         </div>
-        <span className="text-foreground text-xl font-bold tracking-tight">
-          Future<span className="text-primary"></span>
-        </span>
-      </div>
 
-      {/* navigation section */}
-      <nav className="ml-auto hidden items-center gap-2 md:flex">
-        <Button
-          variant="link"
-          className="text-muted-foreground hover:text-primary cursor-pointer"
-        >
-          Styles
-        </Button>
-        <Button
-          variant="link"
-          className="text-muted-foreground hover: text-primary cursor-pointer"
-        >
-          Guide
-        </Button>
-        <Button
-          variant="link"
-          className="text-muted-foreground hover:text-primary cursor-pointer"
-        >
-          FAQ
-        </Button>
-      </nav>
+        {/* navigation section */}
+        <nav className="mr-5 ml-auto hidden items-start gap-2 md:flex">
+          <Button
+            variant="link"
+            className="text-muted-foreground hover:text-primary cursor-pointer text-base transition-colors duration-600 ease-in-out"
+          >
+            Styles
+          </Button>
+          <Button
+            variant="link"
+            className="text-muted-foreground hover:text-primary cursor-pointer text-base"
+          >
+            Guide
+          </Button>
+          <Button
+            variant="link"
+            className="text-muted-foreground hover:text-primary cursor-pointer text-base"
+          >
+            FAQ
+          </Button>
+        </nav>
 
-      {/* login section */}
-      <div className="flex items-center gap-3">
-        <Button
-          variant="ghost"
-          size="sm"
-          className="hidden cursor-pointer sm:inline-flex"
-        >
-          Log In
-        </Button>
+        {/* login section */}
+        <div className="flex items-center gap-3 text-base">
+          <Button
+            variant="default"
+            size="lg"
+            className="text-muted-foreground hidden cursor-pointer bg-transparent text-base hover:text-blue-500 sm:inline-flex"
+          >
+            Log In
+          </Button>
 
-        <Button
-          variant="default"
-          size="sm"
-          className="cursor-pointer rounded-full px-6 font-semibold shadow-sm"
-        >
-          Join now
-        </Button>
-      </div>
-    </header>
+          <Button
+            variant="default"
+            size="lg"
+            className="cursor-pointer rounded-full border-none bg-blue-500 px-8 py-4 text-base font-semibold shadow-sm ring-0 transition-all duration-500 ease-in-out hover:bg-transparent hover:text-blue-500 hover:shadow-md"
+          >
+            Join now
+          </Button>
+        </div>
+      </header>
+    </div>
   );
 }
 
